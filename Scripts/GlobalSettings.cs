66c19b95c886572f6bbe340526b06d32dee8a03e
Maroxis <wola1997@interia.pl>
2020-07-22 19:59:43 +0200

    Initial commit

---
+++ Scripts/GlobalSettings.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSettings : MonoBehaviour
+{
+    [Range(0, 500)]
+    public int boidAmmount;
+    private int prevAmmount;
+    [Range(10f, 200f)]
+    public float speed;
+    private float prevSpeed;
+    [Range(1f, 50f)]
+    public float sensRadius;
+    private float prevRad;
+
+    private GameObject generator;
+    private GenerateBoids genBoids;
+    public List<GameObject> boids;
+
+    public void Init()
+    {
+        boidAmmount = 100;
+        prevAmmount = 0;
+        prevSpeed = speed = 50f;
+        prevRad = sensRadius = 10f;
+
+        generator = GameObject.Find("Generator");
+        genBoids = generator.GetComponent<GenerateBoids>();
+        boids = genBoids.Generate(boidAmmount,prevAmmount,speed,sensRadius);
+
+        prevAmmount = boidAmmount;
+    }
+
+
+    public bool CheckForUpdate()
+    {
+        bool change = false;
+        if (boidAmmount > prevAmmount)
+        {
+            boids = genBoids.Generate(boidAmmount, prevAmmount, speed, sensRadius);
+            prevAmmount = boidAmmount;
+            change = true;
+        }
+        else if (boidAmmount < prevAmmount)
+        {
+            boids = genBoids.Remove(prevAmmount - boidAmmount);
+            prevAmmount = boidAmmount;
+            change = true;
+        }
+
+        if(speed != prevSpeed && boidAmmount > 0)
+        {
+            for (int i = 0; i < boidAmmount; i++)
+            {
+                BoidBehaviour boiBeh = boids[i].GetComponent<BoidBehaviour>();
+                boiBeh.speed = speed;
+            }
+            prevSpeed = speed;
+            change = true;
+        }
+        if (sensRadius != prevRad && boidAmmount > 0)
+        {
+            for (int i = 0; i < boidAmmount; i++)
+            {
+                BoidBehaviour boiBeh = boids[i].GetComponent<BoidBehaviour>();
+                boiBeh.sensRadius = sensRadius;
+            }
+            prevRad = sensRadius;
+            change = true;
+        }
+        return change;
+    }
+}
---
+++ Scripts/Main.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private List<GameObject> boids = new List<GameObject>();
+    private List<BoidBehaviour> boidsBeh = new List<BoidBehaviour>();
+    private GlobalSettings settings;
+    //public ComputeShader compute;
+
+    private int stTurn = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        settings = GameObject.Find("Settings").GetComponent<GlobalSettings>();
+        settings.Init();
+        boids = settings.boids;
+        boidsBeh.Clear();
+        for (int i = 0; i < boids.Count; i++)
+        {
+            BoidBehaviour beh = boids[i].GetComponent<BoidBehaviour>();
+            boidsBeh.Add(beh);
+        }
+
+        //int kernel = compute.FindKernel("CSMain");
+    }
+    void CheckSettings()
+    {
+       bool update = settings.CheckForUpdate();
+        if (update)
+        {
+            boids = settings.boids;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (stTurn % 10 == 0)
+        {
+            CheckSettings();
+            stTurn = 1;
+        }
+        else stTurn++;
+
+        for(int i = 0; i < boids.Count; i++)
+        {
+            BoidBehaviour boidBeh = boids[i].GetComponent<BoidBehaviour>();
+            boidBeh.Calculate();
+        }
+    }
+}
